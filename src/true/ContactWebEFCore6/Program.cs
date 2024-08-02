fae7c08e44bab75029315ce64a8de7333288152c
allisonbolen <allybolen629@gmail.com>
2024-08-02 15:45:07 -0400

    migrate

--- src/true/ContactWebEFCore6/Program.cs
+++ src/true/ContactWebEFCore6/Program.cs
@@ -20,22 +20,22 @@ builder.Services.AddDbContext<MyContactManagerDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-//TODO: Add this back in when you have a database connection to perform migrations on startup
-//var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-//    .UseSqlServer(connectionString)
-//    .Options;
-//using (var context = new ApplicationDbContext(contextOptions))
-//{
-//    context.Database.Migrate();
-//}
+// TODO: Add this back in when you have a database connection to perform migrations on startup
+var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+   .UseSqlServer(connectionString)
+   .Options;
+using (var context = new ApplicationDbContext(contextOptions))
+{
+   context.Database.Migrate();
+}
 
-//var contextOptions2 = new DbContextOptionsBuilder<MyContactManagerDbContext>()
-//    .UseSqlServer(mcmdContext)
-//    .Options;
-//using (var context = new MyContactManagerDbContext(contextOptions2))
-//{
-//    context.Database.Migrate();
-//}
+var contextOptions2 = new DbContextOptionsBuilder<MyContactManagerDbContext>()
+   .UseSqlServer(mcmdContext)
+   .Options;
+using (var context = new MyContactManagerDbContext(contextOptions2))
+{
+   context.Database.Migrate();
+}
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
